110abf58eba7ac133dd1039709b1cc2938688b17
Brian Taylor Johnson <brian.johnson@cigna.com>
2019-10-14 23:44:26 -0400

    Heroku deploy

---
+++ client/build/precache-manifest.728cbe948624df8cb1b0cfc4d567ff77.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2301defcc51fbba5ea65",
+    "url": "/static/js/main.af26c341.chunk.js"
+  },
+  {
+    "revision": "118d1ce1aea72a33a0b1",
+    "url": "/static/js/2.e31c8598.chunk.js"
+  },
+  {
+    "revision": "2301defcc51fbba5ea65",
+    "url": "/static/css/main.76eb7831.chunk.css"
+  },
+  {
+    "revision": "2c2bc5784fa646970c5768e9227d9f00",
+    "url": "/index.html"
+  }
+];
